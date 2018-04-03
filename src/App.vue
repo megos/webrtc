7de5f7275238e7d7b9bd12e17e80a559cbba52af
megos <gmegos.27@gmail.com>
2018-04-03 15:16:00 +0900

    Remove not use data

--- src/App.vue
+++ src/App.vue
@@ -5,24 +5,3 @@
     </v-content>
   </v-app>
 </template>
-
-<script>
-export default {
-  data () {
-    return {
-      clipped: false,
-      drawer: true,
-      fixed: false,
-      items: [{
-        icon: 'bubble_chart',
-        title: 'Inspire'
-      }],
-      miniVariant: false,
-      right: true,
-      rightDrawer: false,
-      title: 'Vuetify.js'
-    }
-  },
-  name: 'App'
-}
-</script>
