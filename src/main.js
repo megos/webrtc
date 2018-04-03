1253302d5cb3c11dd0e7b112a04ad9f30827073e
megos <gmegos.27@gmail.com>
2018-04-03 10:58:24 +0900

    Add card

--- src/main.js
+++ src/main.js
@@ -6,6 +6,7 @@ import router from './router'
 import {
   Vuetify,
   VApp,
+  VCard,
   VNavigationDrawer,
   VFooter,
   VList,
@@ -22,6 +23,7 @@ import '../node_modules/vuetify/src/stylus/app.styl'
 Vue.use(Vuetify, {
   components: {
     VApp,
+    VCard,
     VNavigationDrawer,
     VFooter,
     VList,
