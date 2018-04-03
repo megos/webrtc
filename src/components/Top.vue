4920dbd7d8a128efae554d358ec965aaea4af10a
megos <gmegos.27@gmail.com>
2018-04-03 15:31:03 +0900

    Add peer open

--- src/components/Top.vue
+++ src/components/Top.vue
@@ -2,6 +2,9 @@
   <v-layout row>
     <v-flex xs12 sm6 offset-sm3>
       <v-card>
+        <v-card-title>
+          Your peer ID is {{peerId}}
+        </v-card-title>
       </v-card>
     </v-flex>
   </v-layout>
@@ -9,6 +12,22 @@
 
 <script>
 export default {
-  props: [ 'name' ]
+  props: [ 'name' ],
+  data () {
+    return {
+      peer: {},
+      peerId: ''
+    }
+  },
+  mounted: function () {
+    this.peer = new Peer(this.name, {
+      key: '',
+      debug: 3
+    })
+
+    this.peer.on('open', () => {
+      this.peerId = this.peer.id
+    })
+  }
 }
 </script>
