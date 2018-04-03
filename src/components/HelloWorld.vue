0c9dcf8d3b1fb651e9a8f95b5baa0996cef32cb1
megos <gmegos.27@gmail.com>
2018-04-03 10:43:21 +0900

    Add textfield

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,20 +1,30 @@
 <template>
   <v-container fluid>
-    <v-slide-y-transition mode="out-in">
-      <v-layout column align-center>
-        <img src="@/assets/logo.png" alt="Vuetify.js" class="mb-5">
-        <blockquote>
-          &#8220;First, solve the problem. Then, write the code.&#8221;
-          <footer>
-            <small>
-              <em>&mdash;John Johnson</em>
-            </small>
-          </footer>
-        </blockquote>
-      </v-layout>
-    </v-slide-y-transition>
+    <v-form>
+      <v-text-field
+        name="hoge"
+        label="Name"
+        :counter="10"
+        required
+      ></v-text-field>
+    </v-form>
   </v-container>
 </template>
+
+<script>
+export default {
+  data () {
+    return {
+      valid: false,
+      name: '',
+      nameRules: [
+        v => !!v || 'Name is required',
+        v => v.length <= 10 || 'Name must be less than 10 characters'
+      ]
+    }
+  }
+}
+</script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
