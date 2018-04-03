22972376a24e977c9de7adf9837a3f605f3a957b
megos <gmegos.27@gmail.com>
2018-04-03 16:05:15 +0900

    Add audio and video

--- src/components/Top.vue
+++ src/components/Top.vue
@@ -11,22 +11,47 @@
 </template>
 
 <script>
+import { key } from '../credentials'
+
 export default {
   props: [ 'name' ],
   data () {
     return {
       peer: {},
-      peerId: ''
+      peerId: '',
+      audios: [],
+      videos: []
     }
   },
   mounted: function () {
     this.peer = new Peer(this.name, {
-      key: '',
+      key: key,
       debug: 3
     })
 
     this.peer.on('open', () => {
       this.peerId = this.peer.id
+    })
+
+    navigator.mediaDevices.enumerateDevices()
+    .then(deviceInfos => {
+      for (let i = 0; i !== deviceInfos.length; ++i) {
+        const deviceInfo = deviceInfos[i]
+        console.log(deviceInfo)
+        if (deviceInfo.kind === 'audioinput') {
+          this.audios.push({
+            text: deviceInfo.label ||
+            'Microphone ' + (this.audios.length + 1),
+            value: deviceInfo.deviceId
+          })
+        } else if (deviceInfo.kind === 'videoinput') {
+          this.videos.push({
+            text: deviceInfo.label ||
+            'Camera ' + (this.videos.length + 1),
+            value: deviceInfo.deviceId
+          })
+        }
+      }
     })
   }
 }
