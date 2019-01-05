50479f12e51cdeece572893560f26d1e3a6c8c61
megos <gmegos.27@gmail.com>
2019-01-05 20:40:34 +0900

    Revert files

---
+++ src/components/Login.vue
@@ -0,0 +1,49 @@
+<template>
+  <v-layout row>
+    <v-flex xs12 sm6 offset-sm3>
+      <v-card>
+        <v-form v-model="valid">
+          <v-card-title>
+            Login
+          </v-card-title>
+          <v-card-text>
+            <v-text-field
+              v-model="name"
+              :rules="nameRules"
+              :counter="10"
+              required
+            ></v-text-field>
+          </v-card-text>
+          <v-card-actions>
+            <v-btn
+              @click="submit"
+              :disabled="!valid"
+            >
+              login
+            </v-btn>
+          </v-card-actions>
+        </v-form>
+      </v-card>
+    </v-flex>
+  </v-layout>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      valid: false,
+      name: '',
+      nameRules: [
+        (v) => !!v || 'Name is required',
+        (v) => v.length <= 10 || 'Name must be less than 10 characters'
+      ]
+    }
+  },
+  methods: {
+    submit () {
+      this.$router.push({ name: 'Top', params: { name: this.name } })
+    }
+  }
+}
+</script>
